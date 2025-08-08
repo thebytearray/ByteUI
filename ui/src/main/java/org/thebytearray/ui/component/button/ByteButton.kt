02d28ff58ba4e74ff4f44bedfa013fad6b1f5a3e
Tamim Hossain <tamimh.dev@gmail.com>
2025-08-08 20:25:42 +0600

    Update Formatting

--- ui/src/main/java/org/thebytearray/ui/component/button/ByteButton.kt
+++ ui/src/main/java/org/thebytearray/ui/component/button/ByteButton.kt
@@ -1,7 +1,9 @@
 package org.thebytearray.ui.component.button
 
+import android.content.res.Configuration
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.defaultMinSize
@@ -16,9 +18,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.UiMode
 import androidx.compose.ui.unit.dp
 import org.thebytearray.ui.theme.Neutral200
 import org.thebytearray.ui.theme.Neutral50
+import org.thebytearray.ui.theme.Neutral600
 import org.thebytearray.ui.theme.Neutral700
 import org.thebytearray.ui.theme.Neutral900
 import org.thebytearray.ui.theme.Typography
@@ -31,10 +35,13 @@ fun ByteOutlinedButton(
     enabled: Boolean = true,
     shape: Shape = RoundedCornerShape(size = 6.dp),
     colors: ButtonColors = ButtonDefaults.outlinedButtonColors(
-        containerColor = Color.Transparent, contentColor = Neutral700
+        containerColor = Color.Transparent,
+        contentColor = if (isSystemInDarkTheme()) Neutral200 else Neutral700
     ),
     elevation: ButtonElevation? = ButtonDefaults.buttonElevation(),
-    border: BorderStroke? = BorderStroke(width = 1.dp, color = Neutral200),
+    border: BorderStroke? = BorderStroke(
+        width = 1.dp, color = if (isSystemInDarkTheme()) Neutral600 else Neutral200
+    ),
     contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
     interactionSource: MutableInteractionSource? = null,
     content: @Composable RowScope.() -> Unit
@@ -63,7 +70,8 @@ fun ByteFilledButton(
     enabled: Boolean = true,
     shape: Shape = RoundedCornerShape(size = 6.dp),
     colors: ButtonColors = ButtonDefaults.filledTonalButtonColors(
-        containerColor = Neutral900, contentColor = Neutral50
+        containerColor = if (isSystemInDarkTheme()) Neutral50 else Neutral900,
+        contentColor = if (isSystemInDarkTheme()) Neutral900 else Neutral50
     ),
     elevation: ButtonElevation? = ButtonDefaults.buttonElevation(),
     border: BorderStroke? = null,
@@ -88,17 +96,43 @@ fun ByteFilledButton(
 }
 
 
-@Preview(showBackground = true, backgroundColor = 0x00000000)
+@Preview(
+    name = "Dark Outlined", showBackground = true, backgroundColor = 0x00000000,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+)
 @Composable
-private fun PreviewByteOutlinedButton() {
+private fun PreviewByteDarkOutlinedButton() {
     ByteOutlinedButton(onClick = {}) {
         Text(text = "Cancel", style = Typography.bodyMedium)
     }
 }
 
-@Preview(showBackground = true, backgroundColor = 0x00000000)
+
+@Preview(name = "Light Outlined", showBackground = true, backgroundColor = 0x00000000)
 @Composable
-private fun PreviewByteFilledButton() {
+private fun PreviewByteLightOutlinedButton() {
+    ByteOutlinedButton(onClick = {}) {
+        Text(text = "Cancel", style = Typography.bodyMedium)
+    }
+}
+
+
+@Preview(name = "Light Filled", showBackground = true, backgroundColor = 0x00000000)
+@Composable
+private fun PreviewByteLightFilledButton() {
+    ByteFilledButton(onClick = {}) {
+        Text(text = "Continue", style = Typography.bodyMedium)
+    }
+}
+
+@Preview(
+    name = "Dark Filled",
+    showBackground = true,
+    backgroundColor = 0x00000000,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+)
+@Composable
+private fun PreviewByteDarkFilledButton() {
     ByteFilledButton(onClick = {}) {
         Text(text = "Continue", style = Typography.bodyMedium)
     }
